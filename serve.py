3715b0e8cd7dd785fea31bb080fde86a188c8849
Hai <51971045+hailindu@users.noreply.github.com>
2024-01-28 16:29:14 -0500

    Add files via upload

--- serve.py
+++ serve.py
@@ -4,17 +4,18 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def stress_cpu():
-    # Start the stress_cpu.py script as a separate process
-    subprocess.Popen(["python3", "stress_cpu.py"])
-    return 'CPU stress test started', 200
-
 @app.route('/', methods=['GET'])
 def home():
     hostname = socket.gethostname()
     private_ip = socket.gethostbyname(hostname)
     return private_ip
+
+@app.route('/', methods=['POST'])
+def stress_cpu():
+    script_path = '/home/ubuntu/UIUC_CCA/stress_cpu.py'
+
+    subprocess.Popen(["python3", script_path])
+    return 'CPU stress test started', 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
